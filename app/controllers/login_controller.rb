6d1ff0e9c015c1dfe0780dc62f45ea36c405a775
Hemali jain <hemali.gjain@tcs.com>
2012-12-20 02:49:06 -0800

    redirect placed at last

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,25 +1,17 @@
 class LoginController < ApplicationController
-	 def login
+	def login
     if params[:name]=="admin" && params[:password]=="admin"
- session[:loggedin_user]=101
-
-      redirect_to :projs
-
+      session[:loggedin_user]=101
     end
     if params[:name]=="project manager" && params[:password]=="project manager"
       session[:loggedin_user]=102
-      redirect_to :projs
-end
-  @users = User.all
- @users.each do |user|
-    
-
-    if params[:name]==user.name && params[:password]==user.password
-       session[:loggedin_user]=user.id
-      redirect_to :projs
-    
-    
     end
+    @users = User.all
+    @users.each do |user|
+      if params[:name]==user.name && params[:password]==user.password
+         session[:loggedin_user]=user.id      
+      end    
+    end
+    redirect_to :projs
   end
-end
 end
